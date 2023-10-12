b913b11b6ef6a5d3fdb4deae31a853b0cd9b49b0
hassan11196 <hassan11196@hotmail.com>
2023-10-12 17:23:02 +0200

    Add jira search and wmcore-micro services

---
+++ jira-search/background.js
@@ -0,0 +1,14 @@
+chrome.contextMenus.create({
+    id: "openJira",
+    title: "Open Jira",
+    contexts: ["selection", "link"],
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "openJira") {
+        const selectedText = info.selectionText.trim(); // Strip leading/trailing whitespace
+        const url = `https://its.cern.ch/jira/issues/?jql=text~${encodeURIComponent(selectedText)}%20AND%20(project%20%3D%20CMSPROD%20OR%20project%20%3D%20CMSCOMPPR)`;
+        chrome.tabs.create({ url: url });
+    }
+});
+
---
+++ wmcore-microservices/background.js
@@ -0,0 +1,28 @@
+chrome.contextMenus.create({
+    id: "openMSPileup",
+    title: "Open MS Pileup",
+    contexts: ["selection"],
+  });
+  
+  chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "openMSPileup") {
+      const selectedText = info.selectionText.trim(); // Strip leading/trailing whitespace
+      const url = `https://cmsweb.cern.ch/ms-pileup/data/pileup?pileupName=${encodeURIComponent(selectedText)}`;
+      chrome.tabs.create({ url: url });
+    }
+  });
+  
+
+  chrome.contextMenus.create({
+    id: "openMSTransferor",
+    title: "Open MS Transferor",
+    contexts: ["selection"],
+  });
+  
+  chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "openMSTransferor") {
+      const selectedText = info.selectionText.trim(); // Strip leading/trailing whitespace
+      const url = `https://cmsweb.cern.ch/ms-transferor/data/info?request=${encodeURIComponent(selectedText)}`;
+      chrome.tabs.create({ url: url });
+    }
+  });
