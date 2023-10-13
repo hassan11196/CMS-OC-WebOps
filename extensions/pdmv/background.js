d8667645ea192ab6025628eb18dc225fe6fcc51d
hassan11196 <hassan11196@hotmail.com>
2023-10-13 17:11:17 +0200

    Add pMp extension

---
+++ extensions/pdmv/background.js
@@ -0,0 +1,14 @@
+chrome.contextMenus.create({
+    id: "opemPMP",
+    title: "Open pMp",
+    contexts: ["selection"],
+  });
+  
+  chrome.contextMenus.onClicked.addListener(function (info, tab) {
+    if (info.menuItemId === "opemPMP") {
+      const selectedText = info.selectionText.trim(); // Strip leading/trailing whitespace
+      const url = `https://cms-pdmv-prod.web.cern.ch/pmp/historical?r=${encodeURIComponent(selectedText)}`;
+      chrome.tabs.create({ url: url });
+    }
+  });
+  
